42bd2ed590dc69ed8e3ee481dded44c5c960a1ef
Jprogram01 <113635286+Jprogram01@users.noreply.github.com>
2022-10-04 14:45:47 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,3 +9,18 @@ Cards randomtest = new Cards();
 int randomnumber = randomtest.NewNum();
 Console.WriteLine(randomnumber);
 Console.WriteLine(Display);
+
+
+
+class GameSystem
+
+
+
+{
+    public void Main()
+    {
+    Console.WriteLine("Welcome to the guessing Game!");
+    
+    int guess = Console.ReadLine();
+    }
+}
