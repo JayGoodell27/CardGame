d946e22f48c4743d6fa64d0c0639020fbd08d538
Jprogram01 <113635286+Jprogram01@users.noreply.github.com>
2022-10-04 13:42:09 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,9 +5,7 @@
 //vavarNumbers test = Numbersnew NumbersNn();:
 Numbers test = new Numbers();
 String Display = test.DisplayPoints();
-
-
-
-
-
-;
+Cards randomtest = new Cards();
+int randomnumber = randomtest.NewNum();
+Console.WriteLine(randomnumber);
+Console.WriteLine(Display);
